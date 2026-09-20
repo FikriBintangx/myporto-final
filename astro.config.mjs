f2720ab1e55fa1a28e6a05d34c83bf0d1c391887
Fikri Bintang <fikribn123@gmail.com>
2026-09-20 19:34:51 +0700

    feat: configure custom domain fikribintang.web.id with SEO, Open Graph & sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-﻿// @ts-check
+// @ts-check
 import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fikribintang.web.id',
   output: 'server',
   adapter: vercel()
 });
